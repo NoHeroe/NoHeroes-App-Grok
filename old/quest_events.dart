35e93ab5a6001f5e40f895464d29b9ad02739dda
NoHeroe <eco.noheroes@gmail.com>
2025-11-30 10:01:50 -0400

    Delete old directory

--- old/quest_events.dart
+++
@@ -1,9 +0,0 @@
-class QuestCompletedEvent {
-  final QuestInstance instance;
-  QuestCompletedEvent(this.instance);
-}
-
-class QuestFailedEvent {
-  final QuestInstance instance;
-  QuestFailedEvent(this.instance);
-}
